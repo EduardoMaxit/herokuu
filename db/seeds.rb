ec1c98b981f5bb7926d09ed8dd64123c5230c433
EduardoMaxit <eduardomaxit5@gmail.com>
2015-11-09 17:01:54 -0300

    mejorr

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Puntaje.find_or_create_by(puntos: 8, hospedaje_id: 1 )
+Puntaje.find_or_create_by(puntos: 5, hospedaje_id: 1 )
+
+
+Comentario.find_or_create_by(texto: "esta desmejorada", hospedaje_id: 1, usuario_id: 2)
+Comentario.find_or_create_by(texto: "Muy buena la casa", hospedaje_id: 1, usuario_id: 2)
+
+
+Respuesta.find_or_create_by(texto: "ES verdad, las paredes un desastre ", comentario_id: 1, usuario_id: 1)
+Respuesta.find_or_create_by(texto: "solo tienen algunos manchones de humedad ", comentario_id: 1, usuario_id: 2)
+Respuesta.find_or_create_by(texto: "Por lo menos no se le llueve agua ", comentario_id: 1, usuario_id: 2)
