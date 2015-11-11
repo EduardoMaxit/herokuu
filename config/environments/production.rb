daf7fbafee2cc2a8488a03cade195f37b326cc86
EduardoMaxit <eduardomaxit5@gmail.com>
2015-11-10 22:58:27 -0300

    confemail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,12 +4,11 @@ Rails.application.configure do
 config.action_mailer.default_url_options = { :host => 'https://whispering-atoll-1202.herokuapp.com' }
 config.action_mailer.delivery_method = :smtp
 config.action_mailer.perform_deliveries = true
-config.action_mailer.raise_delivery_errors = false
+config.action_mailer.raise_delivery_errors = true
 config.action_mailer.default :charset => "utf-8"
 config.action_mailer.smtp_settings = {
 address: "smtp.gmail.com",
 port: 587,
-domain: ENV["http://gmail.com"],
 authentication: "plain",
 enable_starttls_auto: true,
 user_name: ENV["eduardomaxit5@gmail.com"],
