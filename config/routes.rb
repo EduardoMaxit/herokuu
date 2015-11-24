e558e992b77b19a00180300f36ab27f8b30e0c2d
EduardoMaxit <eduardomaxit5@gmail.com>
2015-11-24 18:40:28 -0300

    mejorrrreewq

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   
 
+  Your::Application.routes.draw do
+  mount LetterOpenerWeb::Engine, at: "/letter_opener"
+  end
+
   get 'fotos/new'
 
   get 'fotos/create'
